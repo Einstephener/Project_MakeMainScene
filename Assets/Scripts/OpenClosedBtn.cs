7cbe1ce6a94f83318f7f3277d4867733b98106ce
Einstephener <kihyuk0201@gmail.com>
2023-12-12 14:26:53 +0900

    feat - 인벤토리, 상태창 버튼 활성화
    - 상태창 아이콘 추가
    - 상태창, 인벤토리 버튼 연결

---
+++ Assets/Scripts/OpenClosedBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenClosedBtn : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject CharacterBtns;
+
+    public void CloseBtn()
+    {
+        Panel.SetActive(false);
+        CharacterBtns.SetActive(true);
+    }
+
+    public void OpenBtn()
+    {
+        Panel.SetActive(true);
+        CharacterBtns.SetActive(false);
+    }
+}
