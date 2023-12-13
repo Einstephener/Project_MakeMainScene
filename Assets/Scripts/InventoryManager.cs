b400579a21975d3644a46525b31187b675ee1d0c
Einstephener <kihyuk0201@gmail.com>
2023-12-13 20:07:55 +0900

    feat - 상점창 아이템칸 프리팹화
    상점창에서 아이템 프리팹으로 구현

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -2,19 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Items
-{
-    woodenSword,
-    woodenShield,
-    bicycleHelmet
-}
+//public enum Items
+//{
+//    woodenSword,
+//    woodenShield,
+//    bicycleHelmet
+//}
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager instance;
     [SerializeField] private GameObject IsEquip;
 
+    public GameObject ShopItemUIPrefabs;
+    public GameObject ShopScreen;
 
-    List<GameObject> HideBtnList = new List<GameObject>(); 
+
+    [Header("ItemSprite")]
+    [SerializeField] private Sprite woodenSwordImage;
+    [SerializeField] private Sprite woodenShieldImage;
+    [SerializeField] public Sprite[] ItemImages;
+    [SerializeField] public Sprite[] InventoryImages;
+
+
+    List<GameObject> HideBtnList = new List<GameObject>();
+
+    public static List<Weapon> weapons = new List<Weapon>
+    {
+        new Weapon("목검", 100, "초보자의 검. 나무로 만들었다.", 10, 0,  5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5),
+        new Weapon("나무 방패", 100, "초보자의 방패. 나무로 만들었다.", 0, 10, 5)
+    };
     private void Awake()
     {
         instance = this;
@@ -22,7 +46,18 @@ public class InventoryManager : MonoBehaviour
 
     private void Start()
     {
-        
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            ShopItemSetting(ShopItemUIPrefabs, ShopScreen);
+        }
+
+    }
+
+    private void ShopItemSetting(GameObject gameObject, GameObject Parent)
+    {
+        GameObject temp = Instantiate(gameObject, Parent.transform.position, Quaternion.identity);
+        temp.transform.SetParent(Parent.transform);
+        temp.SetActive(true);
     }
 
 }
--- Assets/Scripts/ItemSet.cs
+++ Assets/Scripts/ItemSet.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
-public class ItemSet 
+public class Item
 {
-    public string itemName;
-    public int itemCost;
-    public int itemStats;
-    public Sprite itemImage;
+    public string ItemName { get; } // 아이템 이름
+    public int ItemGold { get; } // 아이템 가격
+    public string ItemDescription { get; } // 아이템 상세 설명
+
+    public Item(string itemName, int itemGold, string itemDescription)
+    {
+        ItemName = itemName;
+        ItemGold = itemGold;
+        ItemDescription = itemDescription;
+    }
+}
+public class Weapon : Item    // 무기 클래스 (아이템을 상속받음)
+{
+    public int ItemStr { get; } // 공격력
+    public int ItemDef { get; } // 공격력
+    public int ItemHp { get; } // 체력
+    public bool isEquipped { get; set; } //장착여부
+
+    public Weapon(string itemName, int itemGold, string itemDescription, int itemStr, int itemDef, int itemHp, bool isEquipped = false)
+        : base(itemName, itemGold, itemDescription)
+    {
+        ItemStr = itemStr;
+        ItemDef = itemDef;
+        ItemHp = itemHp;
+    }
+
 }
